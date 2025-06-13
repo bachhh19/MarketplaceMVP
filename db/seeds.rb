3ebd75d5bd321be19c4cb13294f308740ad72685
Omar Bachar <omarbachar.ob@gmail.com>
2025-06-13 22:26:58 +0200

    ready to go

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,41 +35,6 @@ User.create!(
 
 puts "Création des produits..."
 
-# User.where(role: "seller").each do |seller|
-#   rand(2..5).times do
-#     Product.create!(
-#       user: seller,
-#       name: Faker::Commerce.product_name,
-#       description: Faker::Lorem.sentence(word_count: 10),
-#       price: Faker::Commerce.price(range: 10..100.0),
-#       image_url: Faker::LoremFlickr.image(size: "200x200", search_terms: ['product'])
-#     )
-#   end
-# end
-
-# dog_product_names = [
-#   "Croquettes au bœuf", "Friandises au saumon", "Jouet en corde tressée",
-#   "Panier moelleux", "Gamelle antidérapante", "Harnais rembourré", "Laisse extensible",
-#   "Tapis de fouille", "Balle rebondissante", "Os en caoutchouc", "Pull en laine pour chien",
-#   "Collier réfléchissant", "Distributeur de croquettes", "Brosse de toilettage",
-#   "Dentifrice canin", "Shampooing hypoallergénique", "Sac de transport", "Gilet de sauvetage",
-#   "Jouet distributeur", "Coupe-griffes ergonomique"
-# ]
-
-# User.where(role: "seller").each do |seller|
-#   rand(30..40).times do
-#     name = dog_product_names.sample
-#     seed = URI.encode_www_form_component(name)
-#     image_url = "https://picsum.photos/seed/#{seed}/200/200"
-#     Product.create!(
-#       user: seller,
-#       name: name,
-#       description: Faker::Lorem.sentence(word_count: 12),
-#       price: Faker::Commerce.price(range: 10..100.0).round(2),
-#       image_url: image_url
-#     )
-#   end
-# end
 product_names = [
   "Lampe d’ambiance violette", "Tasse en céramique lavande", "Bougie parfumée lilas",
   "Carnet de notes mauve", "Écharpe en soie prune", "Coussin velours violet",
@@ -80,11 +45,10 @@ product_names = [
 ]
 
 User.where(role: "seller").each do |seller|
-  rand(30..40).times do
+  rand(6..7).times do
     name = product_names.sample
-    seed = URI.encode_www_form_component("violet_" + name)  # ajout du préfixe violet pour la cohérence
+    seed = URI.encode_www_form_component(name)
 
-    # URL image Picsum
     image_url = "https://picsum.photos/seed/#{seed}/200/200"
 
     Product.create!(
@@ -96,6 +60,5 @@ User.where(role: "seller").each do |seller|
     )
   end
 end
-
 
 puts "Seed terminé !"
